01c9edb979c14dd2e4c82d05325e7dd8e1e76db5
raysaleide <73722459+raysaleide@users.noreply.github.com>
2020-11-22 21:41:14 -0300

    Add files via upload

---
+++ Dicionario_açoes.py
@@ -0,0 +1,27 @@
+def best_stock(data):
+    # Insira seu código aqui
+    return max(data, key=data.get)
+
+
+
+# Não escreva nada abaixo dessa linha
+if __name__ == '__main__':
+    print("Example:")
+    print(best_stock({
+        'CAC': 10.0,
+        'ATX': 390.2,
+        'WIG': 1.2
+    }))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert best_stock({
+        'CAC': 10.0,
+        'ATX': 390.2,
+        'WIG': 1.2
+    }) == 'ATX', "First"
+    assert best_stock({
+        'CAC': 91.1,
+        'ATX': 1.01,
+        'TASI': 120.9
+    }) == 'TASI', "Second"
+    print('Terminou')
